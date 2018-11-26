d17024ae6da0abc5f2629657f750c738013d858c
anyruizd <anyruizd@gmail.com>
2018-11-26 15:50:36 -0500

    Add challenge 6

---
+++ challenges/6.js
@@ -0,0 +1,17 @@
+/*
+Corrige la condición para que el programa ejecute la condición adecuadamente y se asigne 
+el valor apropiado al resultado.
+
+*/
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x = y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
