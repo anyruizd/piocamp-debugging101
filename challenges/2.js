cb926cebfba598cb510bc5d4de4281f4dc85e786
anyruizd <anyruizd@gmail.com>
2018-11-26 15:01:12 -0500

    Add challenge 2

---
+++ challenges/2.js
@@ -0,0 +1,10 @@
+/*
+Añade dos console.log() para chequear el tipo de cada una de las variables 
+siete y tres en el código.
+
+*/
+
+let siete = 7;
+let tres = "3";
+console.log(siete + tres);
+// Tu código va aquí
