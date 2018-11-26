a9c8891a41d3933ae9c2690bed17f501fa83de95
anyruizd <anyruizd@gmail.com>
2018-11-26 15:07:25 -0500

    Add third challenge

---
+++ challenges/3.js
@@ -0,0 +1,9 @@
+/*
+Corrije los dos errores de tipeo en el código para que funcione el cálculo de 
+netWorkingCapital.
+*/
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
