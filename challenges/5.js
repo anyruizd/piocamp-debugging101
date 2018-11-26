58b1b349bd4813d90c7eae0dd251a3a990e1e8ec
anyruizd <anyruizd@gmail.com>
2018-11-26 15:35:16 -0500

    Add challenge 5

---
+++ challenges/5.js
@@ -0,0 +1,7 @@
+/*
+Arregla la cadena para que use comillas diferentes para el valor href, o para que escape 
+las existentes. Mantén las comillas dobles alrededor de toda la cadena.
+*/
+
+let innerHtml = "<p>Click here to <a href="#Home">return home</a></p>";
+console.log(innerHtml);
