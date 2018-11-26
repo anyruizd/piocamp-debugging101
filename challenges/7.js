65b1d13d7751a3dce8dd11ff6fc64686aa75cc94
anyruizd <anyruizd@gmail.com>
2018-11-26 15:59:28 -0500

    Add challenge 7

---
+++ challenges/7.js
@@ -0,0 +1,13 @@
+/*
+Corrige el código para que el resultado de la variable result sea igual al valor retornado 
+al llamar a la función getNine.
+*/
+
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine;
+console.log(result);
