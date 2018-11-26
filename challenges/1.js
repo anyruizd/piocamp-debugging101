2b5b7656f4d77b683b813f00b92536036188de68
anyruizd <anyruizd@gmail.com>
2018-11-26 14:45:47 -0500

    Add challenge 1

---
+++ challenges/1.js
@@ -0,0 +1,12 @@
+/*
+Usa el método console.log() para imprimir el valor de la variable a en donde se indica en el 
+código.
+*/
+
+let a = 5;
+let b = 1;
+a++;
+// Tu código va aquí
+
+let sumAB = a + b;
+console.log(sumAB);
