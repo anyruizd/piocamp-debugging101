fcb57910f598ad188560768c26156be56d22bbf9
anyruizd <anyruizd@gmail.com>
2018-11-26 15:15:49 -0500

    Add challenge 4

---
+++ challenges/4.js
@@ -0,0 +1,7 @@
+/*
+Corrige los pares faltantes en el código.
+*/
+
+let myArray = [1, 2, 3;
+let arraySum = myArray.reduce((previous, current =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
